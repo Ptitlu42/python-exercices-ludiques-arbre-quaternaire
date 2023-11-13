46ee39038c60aedfe827b8616b4bfc2cc9ea18a0
Ptitlu42 <lucas.beyer@gmx.fr>
2023-11-13 22:01:12 +0100

    rename

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ptitlu/Documents/Cours/Python/PYTHON EXO FINIS PAS TOUCHER/Python/python-exercices-ludiques-arbre-quaternaire/env"
+set -gx VIRTUAL_ENV "/home/ptitlu/Documents/Cours/Python/python-exercices-ludiques-arbre-quaternaire/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
